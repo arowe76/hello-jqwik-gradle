c1cbb6681f7db9772f124b7d839ef342b9402559
Andrew Rowe <arowe@luc.edu>
2026-02-02 19:06:45 -0600

    Property #3

--- src/test/java/hw/PadStartProperties.java
+++ src/test/java/hw/PadStartProperties.java
@@ -3,7 +3,9 @@ package hw;
 import static org.junit.jupiter.api.Assertions.*;
 
 import net.jqwik.api.*;
+import net.jqwik.api.Property;
 import net.jqwik.api.constraints.*;
+
 
 class PadStartProperties {
 
@@ -34,10 +36,14 @@ class PadStartProperties {
      * Property #3 - Original string is always a suffix
      */
 
-
-
-
-
-
+    @Property
+    boolean originalIsSuffix(
+            @ForAll String s,
+            @ForAll @IntRange(min = 0, max = 200) int n,
+            @ForAll String f
+    ) {
+        String out = PadStart.padStart(s, n, f);
+        return out.endsWith(f);
+    }
 }
 
